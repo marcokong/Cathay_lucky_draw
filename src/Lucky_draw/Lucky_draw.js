4f6b0168bdff5deb407f4756f7b3e14dd5e45d73
marcokong <marcokong3714@gmail.com>
2021-11-14 02:37:20 +0800

    add lucky draw logic

--- src/Lucky_draw/Lucky_draw.js
+++ src/Lucky_draw/Lucky_draw.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import logo from '../pic.png';
 import cathay_logo from '../cathay_logo.svg';
@@ -7,15 +7,47 @@ import './Lucky_draw.css';
 
 const LuckyDraw = (props) => {
     //const [prizes, setPrizes] = useState(props.prizes)
-    let [chance, setChance] = useState(5)
 
     let navigate = useNavigate();
 
+    const [chance, setChance] = useState(localStorage.getItem('chance') || 5);
+    /* 
+    useEffect(() => {
+        const clickHandler = () => {
+            setChance(4);
+            navigate("/result");
+            console.log(chance);
+        };
+    });
+
+    function usePersistedState(key, defaultValue) {
+        const [chance, setChance] = useState(localStorage.getItem('chance') || defaultValue);
+        useEffect(() => {
+            localStorage.setItem(key, chance);
+        }, [key, chance]);
+        return [chance, setChance];
+    }
+    */
+
     const clickHandler = () => {
         setChance(chance - 1);
+        localStorage.setItem('chance', chance - 1)
         navigate("/result");
-        console.log(chance);
+    };
+
+    const returnHandler = () => {
+        setChance(chance - 1);
+        localStorage.setItem('chance', chance - 1)
     }
+
+    useEffect(() => {
+        if (chance < 0) {
+            setChance(5);
+            localStorage.setItem('chance', 5)
+        }
+    }, [chance]
+    );
+
 
     const prizeItems = props.prizes.map((prize) =>
         <Card.Text>- {prize}</Card.Text>
@@ -47,7 +79,12 @@ const LuckyDraw = (props) => {
                 <Row>
                     <Col>Lottery chances: {chance}</Col>
                 </Row>
-                <Button onClick={clickHandler}>Draw</Button>
+                <div>
+                    {chance == 0
+                        ? <Button onClick={returnHandler}>Shop more to have lucky draw</Button>
+                        : <Button onClick={clickHandler}>Draw</Button>
+                    }
+                </div>
             </Container>
         </div>
     );
--- src/Lucky_draw_result/Lucky_draw_result.js
+++ src/Lucky_draw_result/Lucky_draw_result.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Alert } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import './Lucky_draw_result.css';
+import congrats from '../airplane.gif';
+import { useNavigate } from "react-router-dom";
+import draw from '../draw'
 
 const LuckyDrawResult = () => {
+    let navigate = useNavigate();
+    let prize = draw();
+
+    const clickHandler = () => {
+        navigate("/");
+    };
 
     return (
         <div>
-            <Alert>Congratulations!</Alert>
-            <Alert>You have got a XXX</Alert>
+            <Card className='congrats-message'>
+                <Card.Img className="congrats-gif" src={congrats} />
+                <Card.Title>
+                    Congratulations!
+                </Card.Title>
+                <Card.Body>
+                    You have won a {prize}
+                </Card.Body>
+            </Card>
+            <Button onClick={clickHandler}>Shop to get more!</Button>
         </div>
     )
 }
